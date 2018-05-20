97b2bca52dfe31b40831f603403af0cc0496b116
packlikez <p-ing@windowslive.com>
2018-05-20 20:48:25 +0700

    miss indent

--- src/ScrollBox.js
+++ src/ScrollBox.js
@@ -31,10 +31,7 @@ export default class ScrollBox extends Component {
   render() {
     let list = [-9,-8,-7,-6,-5,-4,-3,-2,-1, 0, 1,2,3,4,5,6,7,8,9]
     list = list.map((item, index) => {
-      return 
-      <List key={index}>
-      {item===0?'Today':moment().add(item, 'days').format("Do MMM")}
-      </List>
+      return <List key={index}>{item===0?'Today':moment().add(item, 'days').format("Do MMM")}</List>
     })
     return (
       <Warpper onWheel={this._onWheel}>
